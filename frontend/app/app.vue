7ae305b8f5509b3f5686307aa5417b7514cd9fa7
alexy103 <alexy103@hotmail.fr>
2026-05-10 19:48:53 +0200

    Add auth and default layouts

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="px-4 md:px-24 lg:px-48 xl:px-64 2xl:px-92">
-    <Navbar class="mb-4" />
-    <main>
-      <NuxtPage />
-    </main>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
